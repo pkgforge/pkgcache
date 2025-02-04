88fcd50c69114b1a3a0e52702c39a30b773b24c7
Azathothas <AjamX101@gmail.com>
2025-02-04 13:32:27 +0545

    gen metadata

--- scripts/github/sync_releases.sh
+++ scripts/github/sync_releases.sh
@@ -137,3 +137,32 @@ pushd "${TMPDIR}" >/dev/null 2>&1
   fi
 popd >/dev/null 2>&1
 #-------------------------------------------------------# 
+
+#-------------------------------------------------------#
+##Metadata
+pushd "${TMPDIR}" >/dev/null 2>&1
+curl -qfsSL "https://meta.pkgforge.dev/pkgcache/${HOST_TRIPLET}.json" -o "${TMPDIR}/${HOST_TRIPLET}.json"
+if [[ "$(jq -r '.[] | .ghcr_pkg' "${TMPDIR}/${HOST_TRIPLET}.json" | wc -l)" -le 20 ]]; then
+  echo -e "\n[-] FATAL: Failed to Fetch pkgcache (${HOST_TRIPLET}) Metadata\n"
+ exit 1
+else
+  sed -E "s|https://api\.ghcr\.pkgforge\.dev/pkgforge/pkgcache/(.*)\?tag=(.*)\&download=(.*)$|https://github.com/pkgforge/pkgcache/releases/download/\1/\2/\3|g" -i "${TMPDIR}/${HOST_TRIPLET}.json"
+  if [[ "$(jq -r '.[] | .ghcr_pkg' "${TMPDIR}/${HOST_TRIPLET}.json" | wc -l)" -gt 20 ]]; then
+    #Funcs
+    generate_checksum() 
+    {
+      b3sum "$1" | grep -oE '^[a-f0-9]{64}' | tr -d '[:space:]' > "$1.bsum"
+    }
+    #To Bita
+     bita compress --input "${HOST_TRIPLET}.json" --compression "zstd" --compression-level "21" --force-create "${HOST_TRIPLET}.cba"
+    #To xz
+     xz -9 -T"$(($(nproc) + 1))" --compress --extreme --keep --force --verbose "${HOST_TRIPLET}.json" ; generate_checksum "${HOST_TRIPLET}.json.xz"
+    #To Zstd
+     zstd --ultra -22 --force "${HOST_TRIPLET}.json" -o "${HOST_TRIPLET}.json.zstd" ; generate_checksum "${HOST_TRIPLET}.json.zstd"
+    #Create & Upload
+     gh release create "metadata" --repo "https://github.com/pkgforge/pkgcache" --title "metadata" --prerelease 2>/dev/null
+     find "${TMPDIR}" -maxdepth 1 -type f -iname "*${HOST_TRIPLET}*" -size +3c -print0 | xargs -0 -P "$(($(nproc)+1))" -I '{}' gh release upload "metadata" --repo "https://github.com/pkgforge/pkgcache" '{}' --clobber
+  fi
+fi
+popd >/dev/null 2>&1
+#-------------------------------------------------------#
